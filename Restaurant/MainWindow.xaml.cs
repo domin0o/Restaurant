2428117b0339e39afcb7899898e31504151137d6
Dominik Skrzydlewski <skrzydlewskijunior@gmail.com>
2023-02-07 23:57:11 +0100

    Stworzenie formatki MainWindow w xaml

--- Restaurant/MainWindow.xaml.cs
+++ Restaurant/MainWindow.xaml.cs
@@ -23,6 +23,52 @@ namespace Restaurant
         public MainWindow()
         {
             InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            string guacamolePrice;
+            for(int i =0; i<5; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >= 3)
+                {
+                    menuItems[i].Breads = new string[]
+                    {
+                        "zwykły rogal", "rogal cebulowy", "rogal z pumpernikla", "rogal ze wszystkim"
+                    };
+                }
+                menuItems[i].Generate();
+            }
+            item1.Text = menuItems[0].Description;
+            price1.Text = menuItems[0].Price;
+            item2.Text = menuItems[1].Description;
+            price2.Text = menuItems[1].Price;
+            item3.Text = menuItems[2].Description;
+            price3.Text = menuItems[2].Price;
+            item4.Text = menuItems[3].Description;
+            price4.Text = menuItems[3].Price;
+            item5.Text = menuItems[4].Description;
+            price5.Text = menuItems[4].Price;
+
+            MenuItem specialMenuItem = new MenuItem()
+            {
+                Proteins = new string[] { "Szynka organiczna", "Burger z grzybów", "Mortadella" },
+                Breads = new string[] { "bułka bezglutenowa", "tortilla", "pita" },
+                Condiments = new string[] { "musztarda diżońska", "sos z miso", "au jus" }
+            };
+            specialMenuItem.Generate();
+
+            item6.Text = specialMenuItem.Description;
+            price6.Text = specialMenuItem.Price;
+
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamolePrice = guacamoleMenuItem.Price;
+
+            guacamole.Text = "Dodaj guacamole za " + guacamolePrice;
         }
     }
 }
